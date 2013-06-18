4ff6c0c10e975ee111b8d9aa51381c2d6085a94e
SAPikachu <sapikachu@gmail.com>
2013-06-18 20:17:05 +0800

    Generalize control script

--- control.py
+++ control.py
@@ -21,13 +21,13 @@ def pairs(value):
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("event")
-    parser.add_argument("ip")
+    parser.add_argument("posargs", nargs="*")
     parser.add_argument("--timeout", type=int, default=500)
-    parser.add_argument("--extra-args", type=pairs)
+    parser.add_argument("--kwargs", type=pairs, default="")
     args = parser.parse_args()
 
     client = ControlClient(config.CONTROL_ENDPOINT, resp_timeout=args.timeout)
-    getattr(client, args.event)(args.ip, **(args.extra_args or {}))
+    getattr(client, args.event)(*args.posargs, **args.kwargs)
 
 
 if __name__ == "__main__":
